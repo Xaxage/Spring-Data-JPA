56c3a11c08660bb6ec7bed7ea34ed3ba401c3380
xaxage <xaxage@gmail.com>
2022-03-06 08:23:53 -0800

    Add JPA tests.

--- src/test/java/com/xaxage/jpaintro/JpaIntroApplicationTests.java
+++ src/test/java/com/xaxage/jpaintro/JpaIntroApplicationTests.java
@@ -1,13 +1,26 @@
 package com.xaxage.jpaintro;
 
+import com.xaxage.jpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JpaIntroApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testBookRepository() {
+        long count = bookRepository.count();
+
+        assertThat(count).isGreaterThan(0);
+    }
 }
---
+++ src/test/java/com/xaxage/jpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,46 @@
+package com.xaxage.jpaintro;
+
+import com.xaxage.jpaintro.domain.Book;
+import com.xaxage.jpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)//So we can order test methods execution
+@DataJpaTest//This brings only minimal to test Spring data JPA stuff
+@ComponentScan(basePackages = {"package com.xaxage.jpaintro.bootstrap"})//TO bring bootstrapping data
+public class SpringBootJpaTestSlice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Commit
+    @Order(1)
+    @Test
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+        bookRepository.save(new Book("Something", "ISBN", "Author"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+
+
+    }
+}
